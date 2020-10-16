ec893a8765e1635ee0268c2856b731082de9e433
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-16 08:41:54 +0300

    added post and defined home and about

--- photos/views.py
+++ photos/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+posts = [
+    {
+        'author':'Anna',
+        'title':'Photo Post 1',
+        'content':'First post content',
+        'date_posted':'April 17,2020'
+    },
+    {
+        'author':'Ruth',
+        'title':'Photo Post 2',
+        'content':'Second post content',
+        'date_posted':'April 24,2020'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Instagram</h1>')
+    context = {
+        'posts':posts
+    }
+    return render(request, 'home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Instagram About</h1>')
+    return render(request, 'about.html',{'title':'About'})
+
+
 
