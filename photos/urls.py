51e5a8c9b746bc062d6b0b1222f2463b72b094b2
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-17 08:04:50 +0300

    added paths in login and register

--- photos/urls.py
+++ photos/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='photos-home'),
-    path('about',views.about, name='photos-about'),
+    path('about/',views.about, name='photos-about'),
+    path('register/',views.about, name='photos-register'),
+    path('login/',views.about, name='photos-login'),
 ]
