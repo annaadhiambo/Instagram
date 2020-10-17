f2df12960191d66d0c13dd953117072994f79612
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-17 15:45:52 +0300

    removed register and login path

--- photos/urls.py
+++ photos/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('',views.home, name='photos-home'),
     path('about/',views.about, name='photos-about'),
-    path('register/',views.about, name='photos-register'),
-    path('login/',views.about, name='photos-login'),
 ]
