9863eb88e5706463cbe7e3eac28ca206a8cb5950
annaadhiambo <annaadhiambo01@gmail.com>
2020-10-19 07:44:17 +0300

    imported listviews

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Username
 
 
@@ -11,11 +13,54 @@ def home(request):
     context = {
         'posts':Post.objects.all()
     }
-    return render(request, 'home.html', context)
+    return render(request, 'photo/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'photo/home.html'
+    context_object_name = 'posts'
+    ordering  = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True 
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin,  DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True 
+        return False
 
 def register(request):
     return render(request, 'users/register.html')
-
 
 
 def search_results(request):
@@ -29,5 +74,5 @@ def search_results(request):
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'photos/search.html',{"message":message})
+        return render(request, 'photo/search.html',{"message":message})
 
